d9968ab9792d685fd4d1ad38eb77bb62d2b10425
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-18 17:44:00 -0600

    attempted twitter scrape

--- tweetag.py
+++ tweetag.py
@@ -1,9 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
 chromedriver_path = "/Users/excalibur/Desktop/100days_python/chromedriver"
 
-driver = webdriver.Chrome(executable_path=chromedriver_path)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 ht1 = "sponsored"
 ht2 = "golf"
@@ -12,13 +14,12 @@ ht2 = "golf"
 
 driver.get(f"https://mobile.twitter.com/search?q=%23{ht1}%20%23{ht2}&src=typed_query&f=live")
 
-# tweets = driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[1]/div/span[1]')
+# tweets = driver.find_element(By.CLASS_NAME, 'css-901oao')
 
 tweets = driver.find_elements(By.TAG_NAME, "span")
 
 for tweet in tweets:
-    print(tweet.text)
+    print(tweet.get_attribute("class"))
 
-# print(tweets)
+# print(tweets.get_attribute("class"))
 
-driver.close()
