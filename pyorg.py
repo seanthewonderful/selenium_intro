3726ca8eccb5b7e57b71bc60089ebc38971b17e8
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-19 12:04:53 -0600

    create dictionary from scraped lists

--- pyorg.py
+++ pyorg.py
@@ -9,3 +9,21 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 driver.get("https://www.python.org")
 
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+for n in range(len(event_times)):
+    events[n] = {
+        "time": event_times[n].text,
+        "name": event_names[n].text
+    }
+
+# for time in event_times:
+#     print(time.text)
+
+# for name in event_names:
+#     print(name.text)
+
+print(events)
+
+driver.quit()
