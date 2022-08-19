56aee47953c7063f94453280d8b636bb081f0d1a
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-19 12:20:54 -0600

    more attempts, twitter may be preventing this

--- tweetag.py
+++ tweetag.py
@@ -14,12 +14,13 @@ ht2 = "golf"
 
 driver.get(f"https://mobile.twitter.com/search?q=%23{ht1}%20%23{ht2}&src=typed_query&f=live")
 
-# tweets = driver.find_element(By.CLASS_NAME, 'css-901oao')
+# tweets = driver.find_element(By.CLASS_NAME, 'r-qvutc0')
 
-tweets = driver.find_elements(By.TAG_NAME, "span")
+tweets = driver.find_elements(By.CSS_SELECTOR, "span .css-901oao")
 
-for tweet in tweets:
-    print(tweet.get_attribute("class"))
+# for tweet in tweets:
+#     print(tweet)
 
 # print(tweets.get_attribute("class"))
-
+print(tweets)
+driver.close()
