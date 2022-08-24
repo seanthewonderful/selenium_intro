d27a8f7feb00d230107b5b1e22f5627c3bc5637f
Sean Fagan <58955260+seanthewonderful@users.noreply.github.com>
2022-08-23 21:42:34 -0600

    fails on 'buyGrandma'

--- cookiebot.py
+++ cookiebot.py
@@ -29,22 +29,15 @@ while True:
         for i in range(len(prices) - 2, -1, -1):
             shop_dict[int((prices[i]).text.split(" ")[-1].replace(",", ""))] = item_ids[i]
             
-        # print(shop_dict)
+        print(shop_dict)
 
         for k, v in shop_dict.items():
             money = int(driver.find_element(By.ID, "money").text.split(" ")[-1].replace(",",""))
+            buy = driver.find_element(By.CSS_SELECTOR, f"[id='{v}']")
+            print(v)
 
             if money > k:
-                buy = driver.find_element(By.ID, v)
                 buy.click()
-                k = int(driver.find_element(By.CSS_SELECTOR, f"#{v} b").text.split(" ")[-1].replace(",", ""))
-                
-                # while money > k:
-                #     print("$: ", money)
-                #     print("K: ", k)
-                #     driver.find_element(By.ID, v).click()
-                #     money = int(driver.find_element(By.ID, "money").text.split(" ")[-1].replace(",",""))
-                #     k = int(driver.find_element(By.CSS_SELECTOR, f"#{v} b").text.split(" ")[-1].replace(",", ""))
 
         five_secs = time.time() + 5
     
@@ -54,66 +47,13 @@ while True:
         print(f"Cookies Per Second: {cps}")
         break
         
-    
-# while time.time() < five_mins:
-#     if money < cursor_price:
-#         pass
-#     while time.time() < five_secs:
-#         cookie.click()
-        
-#     while money > 123456789:
-#         time_machine.click()
-        
-#     while money > 1000000:
-#         portal.click()
-        
-#     while money > 50000:
-#         alchemy_lab.click()
-        
-#     while money > 7000:
-#         shipment.click()
-        
-#     while money > 2000:
-#         mine.click()
-        
-#     while money > 500:
-#         factory.click()
-        
-#     while money > 500:
-#         grandma.click()
-        
-#     while money > 100:
-#         cursor.click()
-    
-#     continue
 
 
-
-
-
-
-
-
-# time_machine = driver.find_element(By.ID, "buyTime machine")
-# time_machine_price = driver.find_element(By.CSS_SELECTOR, "#buyTime machine b").text.split(" ")[-1].replace(",", "")
-
-# portal = driver.find_element(By.ID, "buyPortal")
-# portal_price = driver.find_element(By.CSS_SELECTOR, "#buyPortal b").text.split(" ")[-1].replace(",", "")
-
-# alchemy_lab = driver.find_element(By.ID, "buyAlchemy lab")
-# # alchemy_lab_price = driver.find_element(By.CSS_SELECTOR, "#buyAlchemy lab b").text.split(" ")[-1].replace(",", "")
-
-# shipment = driver.find_element(By.ID, "buyShipment")
-# shipment_price = driver.find_element(By.CSS_SELECTOR, "#buyShipment b").text.split(" ")[-1].replace(",", "")
-
-# mine = driver.find_element(By.ID, "buyMine")
-# mine_price = driver.find_element(By.CSS_SELECTOR, "#buyMine b").text.split(" ")[-1].replace(",", "")
-
-# factory = driver.find_element(By.ID, "buyFactory")
-# factory_price = driver.find_element(By.CSS_SELECTOR, "#buyFactory b").text.split(" ")[-1].replace(",", "")
-
-# grandma = driver.find_element(By.ID, "buyGrandma")
-# grandma_price = driver.find_element(By.CSS_SELECTOR, "#buyGrandma b").text.split(" ")[-1].replace(",", "")
-
-# cursor = driver.find_element(By.ID, "buyCursor")
-# cursor_price = driver.find_element(By.CSS_SELECTOR, "#buyCursor b").text.split(" ")[-1]
+                # k = int(driver.find_element(By.CSS_SELECTOR, f"#{v} b").text.split(" ")[-1].replace(",", ""))
+                
+                # while money > k:
+                #     print("$: ", money)
+                #     print("K: ", k)
+                #     driver.find_element(By.ID, v).click()
+                #     money = int(driver.find_element(By.ID, "money").text.split(" ")[-1].replace(",",""))
+                #     k = int(driver.find_element(By.CSS_SELECTOR, f"#{v} b").text.split(" ")[-1].replace(",", ""))
